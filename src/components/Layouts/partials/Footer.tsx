3dd3fa0862e3e1f0dd69ae2511b517accbe0e73f
IRONDESK <zerozerobase@gmail.com>
2022-10-14 00:41:37 +0900

    [Feat] header, footer 구현

--- src/components/Layouts/partials/Footer.tsx
+++ src/components/Layouts/partials/Footer.tsx
@@ -1,7 +1,11 @@
 import React from "react"
+import styled from "@emotion/styled"
 
 function Footer() {
-  return <div>Footer</div>
+  return <Container>© 2022, SON SOOCHUL</Container>
 }
 
+const Container = styled.footer`
+  padding: 24px 20px 40px;
+`
 export default Footer
--- src/components/Layouts/partials/Header.tsx
+++ src/components/Layouts/partials/Header.tsx
@@ -1,7 +1,53 @@
 import React from "react"
+import styled from "@emotion/styled"
+import Link from "next/link"
 
 function Header() {
-  return <div>Header</div>
+  return (
+    <Container>
+      <Name>
+        AWESOME
+        <br />
+        FOOD STORE
+      </Name>
+      <Menu>
+        <li>
+          <Link href="/about">
+            <span>about</span>
+          </Link>
+        </li>
+        <li>
+          <Link href="/store">
+            <span>store</span>
+          </Link>
+        </li>
+      </Menu>
+    </Container>
+  )
 }
+
+const Container = styled.header`
+  display: flex;
+  padding: 24px 20px;
+  justify-content: space-between;
+  align-items: center;
+`
+const Name = styled.h1`
+  font-size: 1.6rem;
+  font-weight: 900;
+`
+const Menu = styled.ul`
+  display: flex;
+  span {
+    cursor: pointer;
+    padding: 12px 20px;
+    &:hover {
+      color: #000;
+    }
+  }
+  &:hover {
+    color: #888;
+  }
+`
 
 export default Header
